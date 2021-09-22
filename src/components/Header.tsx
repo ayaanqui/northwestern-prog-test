eeb5cbecb6af1b48eb8bb7e5339fc6a4dcc81e2a
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-09-22 15:30:03 -0500

    Add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/system"
+
+const Header = () => {
+  return (
+    <Box textAlign="center" marginBottom="3em">
+      <h1>Manage My Items</h1>
+    </Box>
+  )
+}
+
+export default Header
