d40801b30cef2050df48e958ca851b9ebe17a592
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-09-22 14:33:48 -0500

    Install nodemon + remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
 import { Button } from "@mui/material"
 
 function App() {
-  return <Button variant="contained">Hello world</Button>
+  return <Button variant="contained">Hello world!</Button>
 }
 
 export default App
