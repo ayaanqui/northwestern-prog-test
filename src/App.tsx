d8265e28ed8f83b472817a7c2d667927c63600e3
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-09-22 16:28:24 -0500

    Add documentation for App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,18 @@ import MyItems from "./components/MyItems"
 const App = () => {
   const [list, setList] = useState<Array<string>>([])
 
+  /**
+   * Given a string `item`, add it to the list
+   * @param item String value
+   */
   const addToList = (item: string) => {
     setList([item, ...list])
   }
 
+  /**
+   * Given a postition in the array `list`, remove the element from the list
+   * @param index Postition of the item in list
+   */
   const removeFromList = (index: number) => {
     setList(list.filter((_, i) => i !== index))
   }
