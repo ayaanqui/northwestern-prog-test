07bf684a61091a17b1f4941c1521342e06204025
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-09-22 16:01:46 -0500

    Add props to AddItem component + update state

--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -1,17 +1,41 @@
 import { Button, Stack, TextField } from "@mui/material"
+import { Dispatch, SetStateAction, useState } from "react"
 
-const AddItem = (props: any) => {
+export interface AddItemProps {
+  addItem: (item: string) => void
+}
+
+const AddItem = ({ addItem }: AddItemProps) => {
+  const [input, setInput] = useState<string>("")
+
+  const updateInput = (event: any) => {
+    setInput(event.target.value)
+  }
+
+  const submit = () => {
+    addItem(input)
+  }
+
   return (
-    <Stack direction="row" spacing={2} justifyContent="space-between" mt="sm">
+    <Stack
+      direction="row"
+      spacing={2}
+      justifyContent="space-between"
+      padding="10px">
       <TextField
         id="outlined-basic"
-        label="Add item..."
+        label="Item"
         variant="outlined"
         fullWidth
         style={{ flex: 3 }}
+        onChange={updateInput}
       />
 
-      <Button variant="contained" fullWidth style={{ flex: 1 }}>
+      <Button
+        variant="contained"
+        fullWidth
+        style={{ flex: 1 }}
+        onClick={submit}>
         Add Item
       </Button>
     </Stack>
